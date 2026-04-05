7c2781ae3abc51c85f5b730f8a930c1f70fda0ca
paruff <paruff@users.noreply.github.com>
2026-04-05 20:40:36 +0000

    deploy: 7120b2297d39f43fdce28fd55d36343fcfdf9e8d

---
+++ assets/js/a7456010.9bb3c831.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkintegral_education_platform=globalThis.webpackChunkintegral_education_platform||[]).push([[1235],{8552(a){a.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
